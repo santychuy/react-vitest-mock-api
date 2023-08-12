d50b9540abc04f95a5b572070aeb8fdfa6b3a928
Santiago Carrasco <jsanticc@hotmail.com>
2023-08-12 13:04:44 -0700

    chore: move vitest config into vite main config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
   base: '/react-vitest-mock-api/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    exclude: ['**/e2e/**', '**/node_modules/**', '**/dist/**'],
+  },
+  plugins: [vanillaExtractPlugin(), react()],
 });
