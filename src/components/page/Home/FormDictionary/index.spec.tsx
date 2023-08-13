080812c51e54733de70933d2fedc3be02acc751a
Santiago Carrasco <jsanticc@hotmail.com>
2023-08-12 17:06:08 -0700

    test: improve test for wait for changing state

--- src/components/page/Home/FormDictionary/index.spec.tsx
+++ src/components/page/Home/FormDictionary/index.spec.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import FormDictionary from '.';
@@ -22,11 +22,13 @@ describe('<FormDictionary />', () => {
     await userEvent.type(input, 'dog');
     userEvent.click(button);
 
-    expect(
-      await screen.findByText(
-        /A mammal, Canis familiaris or Canis lupus familiaris./i,
-      ),
-    ).toBeInTheDocument();
+    await waitFor(async () => {
+      expect(
+        await screen.findByText(
+          /A mammal, Canis familiaris or Canis lupus familiaris./i,
+        ),
+      ).toBeInTheDocument();
+    });
   });
 
   it('should type in input a random word, search and get error', async () => {
@@ -38,7 +40,9 @@ describe('<FormDictionary />', () => {
     await userEvent.type(input, 'fdsakfjk');
     userEvent.click(button);
 
-    expect(await screen.findByText(/Word not found/i)).toBeInTheDocument();
+    await waitFor(async () => {
+      expect(await screen.findByText(/Word not found/i)).toBeInTheDocument();
+    });
   });
 
   it('should be disabled if input is empty', () => {
@@ -49,33 +53,42 @@ describe('<FormDictionary />', () => {
     expect(button).toBeDisabled();
   });
 
-  it('should be disabled if input is invalid after entering numbers', () => {
+  it('should be disabled if input is invalid after entering numbers', async () => {
     render(<FormDictionary />);
 
     const input = screen.getByRole('textbox');
     const button = screen.getByRole('button', { name: /search/i });
 
-    userEvent.type(input, '123');
-    expect(button).toBeDisabled();
+    await userEvent.type(input, '123');
+
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+    });
   });
 
-  it('should be disabled if input is invalid after entering special characters', () => {
+  it('should be disabled if input is invalid after entering special characters', async () => {
     render(<FormDictionary />);
 
     const input = screen.getByRole('textbox');
     const button = screen.getByRole('button', { name: /search/i });
 
-    userEvent.type(input, '!!!');
-    expect(button).toBeDisabled();
+    await userEvent.type(input, '!!!');
+
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+    });
   });
 
-  it('should be disabled if input is invalid after entering spaces', () => {
+  it('should be disabled if input is invalid after entering spaces', async () => {
     render(<FormDictionary />);
 
     const input = screen.getByRole('textbox');
     const button = screen.getByRole('button', { name: /search/i });
 
-    userEvent.type(input, '   ');
-    expect(button).toBeDisabled();
+    await userEvent.type(input, '   ');
+
+    await waitFor(() => {
+      expect(button).toBeDisabled();
+    });
   });
 });
